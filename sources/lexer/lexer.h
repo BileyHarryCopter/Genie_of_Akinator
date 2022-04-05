7e7d097c199db2010b330be9b3a4c15be083763b
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-04-06 01:10:22 +0300

    simple dividing by files

--- sources/lexer/lexer.h
+++ sources/lexer/lexer.h
@@ -5,30 +5,17 @@
 #define CAP_INIT 20
 #define STR_INIT 150
 
-//  in init.h
 typedef char* data_t;
 
 typedef enum braces       {LBRACE   = 10, RBRACE} braces;
 
 typedef enum lexem_kind_t {BRACKET  = 20, QUESTION, ANSWER} lexem_kind_t;
 
-typedef union lexem
-{
-    braces        brac;
-    data_t        data;
-} lexem;
+typedef union lexem lexem;
 
-typedef struct lexem_t
-{
-    lexem_kind_t  kind;
-    lexem         lexm;
-} lexem_t;
+typedef struct lexem_t lexem_t;
 
-typedef struct lex_array_t
-{
-    lexem_t*      lexems;
-    unsigned      capacity, size;
-} lex_array_t;
+typedef struct lex_array_t lex_array_t;
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 int StrDel (char *str);
