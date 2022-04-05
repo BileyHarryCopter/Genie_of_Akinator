cc9a88d41ff6a92adea1a489a057aa3d1415534d
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-04-06 01:11:21 +0300

    incapsulation of lexem structure

---
+++ sources/includes/initlex.h
@@ -0,0 +1,22 @@
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+        //  Incapsulation of initialization the structure of lexems //  
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+typedef union lexem
+{
+    braces        brac;
+    data_t        data;
+} lexem;
+
+typedef struct lexem_t
+{
+    lexem_kind_t  kind;
+    lexem         lexm;
+} lexem_t;
+
+typedef struct lex_array_t
+{
+    lexem_t*      lexems;
+    unsigned      capacity, size;
+} lex_array_t;
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
+//===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
