2f99b3fb0b0073afe9ad8193cb5db7f02f221e41
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-04-18 15:06:12 +0300

    data

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include "sources/game/game.h"
+#include "AkiTree/AkiTree.h"
+#include "sources/includes/initlex.h"
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 int main (void)
 {
-    RunAkinator ("sources/data/input.txt", "sources/data/output.txt");
+    RunAkinator ("AkiTree/AkiTree.txt", "sources/data/output.txt");
     return 0;
 }
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-            // Listen to "Who Wants to Live Forever" by Queen!!!    //
+            // Listen to "Who Wants to Live Forever" by Queen!!! //
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
